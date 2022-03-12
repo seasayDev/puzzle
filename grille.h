7b2b881e7a8b96755d7ed7ac243ce198529774a4
Saliou Cisse <cisse.saliou@courrier.uqam.ca>
2022-03-12 12:24:41 -0500

    feat(grille): definition d'entete de la grille

---
+++ grille.h
@@ -0,0 +1,10 @@
+/**
+ * grille.h contient l'ensembe des règles d'affaires 
+ * permettant de traiter une grille dans un tableau.
+ * 
+ * @author Saliou Cisse (cisse.saliou@courrier.uqam.ca)
+ * @version 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
