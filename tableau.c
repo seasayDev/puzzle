0420063e975ca8f9083dbfe9bb26994bfda9a330
Saliou Cisse <cisse.saliou@courrier.uqam.ca>
2022-03-12 14:08:27 -0500

    feat(tableau): implementation de la methode de lecture

--- tableau.c
+++ tableau.c
@@ -1,2 +1,22 @@
 #include "util.h"
 
+
+
+
+
+void lire_tableau( FILE* fichier, tableau tableau ) {
+   char ligne[TAMPON];
+
+   for ( int v = 0; v < HAUTEUR; v++ ) {
+      fgets ( ligne, TAMPON, fichier );
+      retirer_espace( ligne );
+
+      for ( int h = 0; h < LARGEUR; h++ ) {
+         tableau[h][v].lettre = ligne[h];
+      }
+   }
+
+
+
+
+
