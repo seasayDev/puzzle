ab38e4690f8e778b89198235516bb291161b468e
Saliou Cisse <cisse.saliou@courrier.uqam.ca>
2022-03-12 11:17:57 -0500

    Definition de l'entete de la bibliotheque util

---
+++ util.h
@@ -0,0 +1,12 @@
+/**
+ *  util.h contient l'ensemble des fonctions utilitaires, des constantes communes 
+ * et bibliothèques utilisées dans les differentes instances du logiciel. 
+ * 
+ * @author Saliou Cisse (cisse.saliou@courrier.uqam.ca)
+ * 
+ * @version 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
