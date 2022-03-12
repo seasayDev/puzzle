57fd08c0c3a5678224819313dfe6914e9ca01777
Saliou Cisse <cisse.saliou@courrier.uqam.ca>
2022-03-12 13:05:15 -0500

    feat(tableau): definition de l'entete du module tableau

---
+++ tableau.h
@@ -0,0 +1,12 @@
+/**
+ * tableau.h contient l'ensemble des régles d'affaires permettant de 
+ * traiter le tableau contenu dans le fichier en entrée.
+ * 
+ * @author Saliou Cisse (cisse.saliou@courrier.uqam.com)
+ * @brief 
+ * @version 2022-02-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
